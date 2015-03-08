16dec207eb8682cd5e42e5a39dc16456fdcf08b6
Jason Rivers <jason@jasonrivers.co.uk>
2015-03-08 22:30:11 +0000

    better build script

--- build.sh
+++ build.sh
@@ -3,9 +3,26 @@ set -e
 
 PATH=node_modules/.bin/:$PATH
 
-mkdir -p css js
+mkdir -p css js build/tmp
 
-coffee -o js coffee/*.coffee
+js_files='
+   node_modules/jquery/dist/cdn/jquery-2.1.3.min.js
+   coffee/ttp-logic.coffee
+'
+
+us='' # names of compiled files
+changed=0
+for f in $js_files; do 
+  u=build/tmp/${f//\//_} # replace / with _
+  us="$us $u"
+  if [ $f -nt $u ]; then
+    changed=1
+    if [ $f != ${f%%.coffee} ]; then echo "compiling $f"; coffee -bcp $f >$u
+    elif [ $f != ${f%%.min.js} ]; then echo "copying $f"; cp $f $u 
+    else echo "cleaning up $f"; <$f sed '/^\(var \w\+ = \)\?require(/d' >$u; fi 
+  fi 
+done
+if [ $changed -eq 1 ]; then echo 'concatenating libs'; cat $us >js/ttp-logic.js; fi 
+
 node-sass -o css sass/*.sass
 
-cp node_modules/jquery/dist/cdn/*min* js/
